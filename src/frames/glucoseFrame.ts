0af06c8f2ba0e77ba90c032eac04611b8108f576
Andreas Oldeskog <142856+aolde@users.noreply.github.com>
2021-10-21 20:31:25 +0200

    change logic for separator between glucose and delta

--- src/frames/glucoseFrame.ts
+++ src/frames/glucoseFrame.ts
@@ -7,9 +7,10 @@ export const glucoseFrame = (data: FrameData): LaMetricFrame => {
 
   const glucose = roundGlucose(data.convertGlucoseUnit(latestEntry.sgv));
   const delta = roundGlucose(data.convertGlucoseUnit(latestEntry.delta));
+  const separator = data.settings.unit === "mmol/L" ? " " : ""; // too tight to use space separator in mg/dL unit
 
   return {
-    text: glucose + " " + (latestEntry.delta >= 0 ? "+" : "") + delta,
+    text: glucose + separator + (latestEntry.delta >= 0 ? "+" : "") + delta,
     icon: getDirectionIconId(latestEntry.direction),
   };
 };
