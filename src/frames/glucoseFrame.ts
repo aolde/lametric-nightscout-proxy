e49dd2192dbf3134879371e33b421a64b51aa98e
Andreas Oldeskog <142856+aolde@users.noreply.github.com>
2021-10-21 21:44:41 +0200

    add warning color (red) when out of range

--- src/frames/glucoseFrame.ts
+++ src/frames/glucoseFrame.ts
@@ -8,9 +8,11 @@ export const glucoseFrame = (data: FrameData): LaMetricFrame => {
   const glucose = roundGlucose(data.convertGlucoseUnit(latestEntry.sgv));
   const delta = roundGlucose(data.convertGlucoseUnit(latestEntry.delta));
   const separator = data.settings.unit === "mmol/L" ? " " : ""; // too tight to use space separator in mg/dL unit
+  const warningColor =
+    glucose < data.settings.lowTarget || glucose > data.settings.highTarget;
 
   return {
     text: glucose + separator + (delta >= 0 ? "+" : "") + delta,
-    icon: getDirectionIconId(latestEntry.direction),
+    icon: getDirectionIconId(latestEntry.direction, warningColor),
   };
 };
