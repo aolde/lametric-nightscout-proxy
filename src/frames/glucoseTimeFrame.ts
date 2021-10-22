977d08e4c6d262a896149590cce923fc4adea97c
Andreas Oldeskog <142856+aolde@users.noreply.github.com>
2021-10-22 13:08:55 +0200

    fix calculation of elapsed minutes

--- src/frames/glucoseTimeFrame.ts
+++ src/frames/glucoseTimeFrame.ts
@@ -2,8 +2,13 @@ import { LaMetricFrame } from "../lametric/LaMetricFrame";
 import { FrameData } from "./FrameData";
 
 export const glucoseTimeFrame = (data: FrameData): LaMetricFrame => {
+  const latestEntryDate = new Date(data.properties.delta.times.recent);
+  const nowDate = new Date();
+  const diffMs = nowDate.getTime() - latestEntryDate.getTime();
+  const elapsedMinutes = Math.round(diffMs / 60000);
+
   return {
-    text: data.properties.delta.elapsedMins + " min",
+    text: elapsedMinutes + " min",
     icon: "1609",
   };
 };
