fc7e2482934a33a8a02a5a95cf433dd819eaff42
Andreas Oldeskog <142856+aolde@users.noreply.github.com>
2023-05-19 21:48:45 +0200

    fix issue with delta missing

--- src/frames/glucoseFrame.ts
+++ src/frames/glucoseFrame.ts
@@ -6,13 +6,15 @@ export const glucoseFrame = (data: FrameData): LaMetricFrame => {
   const latestEntry = data.entries[0];
 
   const glucose = roundGlucose(data.convertGlucoseUnit(latestEntry.sgv));
-  const delta = roundGlucose(data.convertGlucoseUnit(latestEntry.delta));
+  const delta = latestEntry.delta
+    ? roundGlucose(data.convertGlucoseUnit(latestEntry.delta))
+    : undefined;
   const separator = data.settings.unit === "mmol/L" ? " " : ""; // too tight to use space separator in mg/dL unit
   const warningColor =
     glucose < data.settings.lowTarget || glucose > data.settings.highTarget;
 
   return {
-    text: glucose + separator + (delta >= 0 ? "+" : "") + delta,
+    text: glucose + (delta ? separator + (delta >= 0 ? "+" : "") + delta : ""),
     icon: getDirectionIconId(latestEntry.direction, warningColor),
   };
 };
